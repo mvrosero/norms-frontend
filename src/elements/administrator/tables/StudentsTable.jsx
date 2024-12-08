55860f95ef0e9c3839cfcf5f3682bccf1ba413fb
Michaela Vera G. Rosero <mvrosero@gbox.ncf.edu.ph>
2024-12-08 17:40:03 +0800

    batch edit student working, but table doesnt reload automatically yet

--- src/elements/administrator/tables/StudentsTable.jsx
+++ src/elements/administrator/tables/StudentsTable.jsx
@@ -5,6 +5,9 @@ import Swal from 'sweetalert2';
 import EditIcon from '@mui/icons-material/Edit';
 import PersonIcon from '@mui/icons-material/Person';
 import BatchStudentsToolbar from '../toolbars/BatchStudentsToolbar';
+import ViewStudentModal from '../modals/ViewStudentModal';
+import EditStudentModal from '../modals/EditStudentModal';
+import "../../../styles/Students.css";
 
 const StudentsTable = () => {
   const [users, setUsers] = useState([]);
@@ -20,7 +23,7 @@ const StudentsTable = () => {
   const fetchUsers = useCallback(async () => {
     try {
       const [userResponse, departmentResponse, programResponse] = await Promise.all([
-        axios.get('http://localhost:9000/students'),
+        axios.get('http://localhost:9000/students-not-archived'),
         axios.get('http://localhost:9000/departments'),
         axios.get('http://localhost:9000/programs'),
       ]);
@@ -59,25 +62,25 @@ const StudentsTable = () => {
   };
 
   // Handle modal to view user details
-  const handleViewUser = (user) => {
+  const handleReadModalShow = (user) => {
     setSelectedUser(user);
     setShowReadModal(true);
-  };
+};
 
-  const handleCloseReadModal = () => setShowReadModal(false);
+  const handleReadModalClose = () => setShowReadModal(false);
 
-  // Handle modal to update user details
-  const handleUpdateUser = (user) => {
+  // Handle modal to edit user details
+  const handleUpdateModalShow = (user) => {
     setSelectedUser(user);
     setShowUpdateModal(true);
-  };
+};
 
-  const handleCloseUpdateModal = () => setShowUpdateModal(false);
+const handleUpdateModalClose = () => setShowUpdateModal(false);
 
   // Handle batch update
   const handleBatchUpdate = (updates) => {
     axios
-      .put('http://localhost:9000/students-multiupdate', {
+      .put('http://localhost:9000/students', {
         student_ids: selectedStudentIds,
         updates,
       })
@@ -94,23 +97,23 @@ const StudentsTable = () => {
   // Render Table
   const renderTable = () => {
     return (
-      <Table striped bordered hover responsive>
+      <Table bordered hover responsive style={{ borderRadius: '20px', marginBottom: '50px', marginLeft: '110px' }}>
         <thead>
           <tr>
-            <th>
+            <th style={{ width: '3%' }}>
               <input
                 type="checkbox"
                 checked={selectAll}
                 onChange={handleSelectAll}
               />
             </th>
-            <th>Student ID</th>
-            <th>Name</th>
-            <th>Year Level</th>
+            <th style={{ width: '10%' }}>ID Number</th>
+            <th>Full Name</th>
+            <th style={{ width: '10%' }}>Year Level</th>
             <th>Department</th>
             <th>Program</th>
-            <th>Status</th>
-            <th>Actions</th>
+            <th style={{ width: '12%' }}>Status</th>
+            <th style={{ width: '11%' }}>Actions</th>
           </tr>
         </thead>
         <tbody>
@@ -123,20 +126,42 @@ const StudentsTable = () => {
                   onChange={() => handleSelectUser(user.student_idnumber)}
                 />
               </td>
-              <td>{user.student_idnumber}</td>
-              <td>{user.full_name}</td>
-              <td>{user.year_level}</td>
-              <td>{user.department_name}</td>
-              <td>{user.program_name}</td>
-              <td>{user.status}</td>
-              <td>
-                <Button variant="info" onClick={() => handleViewUser(user)}>
-                  <PersonIcon />
-                </Button>
-                <Button variant="warning" onClick={() => handleUpdateUser(user)}>
-                  <EditIcon />
-                </Button>
-              </td>
+                <td>{user.student_idnumber}</td>
+                <td>{`${user.first_name} ${user.middle_name || ''} ${user.last_name} ${user.suffix || ''}`}</td>
+                <td>{user.year_level}</td>
+                <td>{departments.find(department => department.department_id === user.department_id)?.department_name || ''}</td>
+                <td>{programs.find(program => program.program_id === user.program_id)?.program_name || ''}</td>
+                <td style={{ textAlign: 'center' }}>
+                        <div style={{
+                            backgroundColor: user.status === 'active' ? '#DBF0DC' : '#F0DBDB',
+                            color: user.status === 'active' ? '#30A530' : '#D9534F',
+                            fontWeight: '600',
+                            fontSize: '14px',
+                            borderRadius: '30px',
+                            padding: '5px 20px',
+                            display: 'inline-flex',
+                            alignItems: 'center',
+                        }}>
+                            <div style={{
+                                width: '8px',
+                                height: '8px',
+                                borderRadius: '50%',
+                                backgroundColor: user.status === 'active' ? '#30A530' : '#D9534F',
+                                marginRight: '7px',
+                            }} />
+                            {user.status}
+                        </div>
+                    </td>                                               
+                    <td>
+                        <div className="d-flex justify-content-around">
+                            <Button className='btn btn-secondary btn-sm' onClick={() => handleReadModalShow(user)}>
+                                <PersonIcon />
+                            </Button>
+                            <Button className='btn btn-success btn-sm' onClick={() => handleUpdateModalShow(user)}>
+                                <EditIcon />
+                            </Button>
+                        </div>
+                    </td>
             </tr>
           ))}
         </tbody>
@@ -154,124 +179,24 @@ const StudentsTable = () => {
       {renderTable()}
 
       {/* View Student Modal */}
-      <Modal show={showReadModal} onHide={handleCloseReadModal}>
+      <Modal show={showReadModal} onHide={handleReadModalClose}>
         <Modal.Header closeButton>
-          <Modal.Title>View Student</Modal.Title>
+            <Modal.Title style={{ marginLeft: '65px' }}>VIEW STUDENT RECORD</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <div>
-            <strong>Full Name:</strong> {selectedUser?.full_name}
-          </div>
-          <div>
-            <strong>Student ID:</strong> {selectedUser?.student_idnumber}
-          </div>
-          <div>
-            <strong>Year Level:</strong> {selectedUser?.year_level}
-          </div>
-          <div>
-            <strong>Department:</strong> {selectedUser?.department_name}
-          </div>
-          <div>
-            <strong>Program:</strong> {selectedUser?.program_name}
-          </div>
-          <div>
-            <strong>Status:</strong> {selectedUser?.status}
-          </div>
+            {selectedUser && <ViewStudentModal user={selectedUser} departments={departments} programs={programs} />}
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleCloseReadModal}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
+     </Modal>
 
-      {/* Update Student Modal */}
-      <Modal show={showUpdateModal} onHide={handleCloseUpdateModal}>
+      {/* Edit Student Modal */}
+    <Modal show={showUpdateModal} onHide={handleUpdateModalClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Update Student</Modal.Title>
+            <Modal.Title style={{ marginLeft: '65px' }}>EDIT STUDENT RECORD</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form
-            onSubmit={(e) => {
-              e.preventDefault();
-              const updates = {
-                year_level: selectedUser.year_level,
-                department_id: selectedUser.department_id,
-                program_id: selectedUser.program_id,
-                status: selectedUser.status,
-              };
-              handleBatchUpdate(updates);
-            }}
-          >
-            <Form.Group controlId="year_level">
-              <Form.Label>Year Level</Form.Label>
-              <Form.Control
-                as="select"
-                value={selectedUser?.year_level}
-                onChange={(e) => setSelectedUser({ ...selectedUser, year_level: e.target.value })}
-              >
-                <option>First Year</option>
-                <option>Second Year</option>
-                <option>Third Year</option>
-                <option>Fourth Year</option>
-                <option>Fifth Year</option>
-              </Form.Control>
-            </Form.Group>
-
-            <Form.Group controlId="department_id">
-              <Form.Label>Department</Form.Label>
-              <Form.Control
-                as="select"
-                value={selectedUser?.department_id}
-                onChange={(e) => setSelectedUser({ ...selectedUser, department_id: e.target.value })}
-              >
-                {departments.map(department => (
-                  <option key={department.department_id} value={department.department_id}>
-                    {department.department_name}
-                  </option>
-                ))}
-              </Form.Control>
-            </Form.Group>
-
-            <Form.Group controlId="program_id">
-              <Form.Label>Program</Form.Label>
-              <Form.Control
-                as="select"
-                value={selectedUser?.program_id}
-                onChange={(e) => setSelectedUser({ ...selectedUser, program_id: e.target.value })}
-              >
-                {programs.map(program => (
-                  <option key={program.program_id} value={program.program_id}>
-                    {program.program_name}
-                  </option>
-                ))}
-              </Form.Control>
-            </Form.Group>
-
-            <Form.Group controlId="status">
-              <Form.Label>Status</Form.Label>
-              <Form.Control
-                as="select"
-                value={selectedUser?.status}
-                onChange={(e) => setSelectedUser({ ...selectedUser, status: e.target.value })}
-              >
-                <option value="active">Active</option>
-                <option value="inactive">Inactive</option>
-                <option value="archived">Archived</option>
-              </Form.Control>
-            </Form.Group>
-
-            <Button variant="primary" type="submit">
-              Update Student
-            </Button>
-          </Form>
+            {selectedUser && <EditStudentModal fetchUsers={fetchUsers} user={selectedUser} departments={departments} programs={programs} handleClose={handleUpdateModalClose} />}
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleCloseUpdateModal}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
+    </Modal>
     </div>
   );
 };
