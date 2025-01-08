4aeb49bee3c1372f174f572be0af3c6747b54a65
Michaela Vera G. Rosero <mvrosero@gbox.ncf.edu.ph>
2025-01-09 00:26:06 +0800

    NORMS top uniform defiances by department chart working

--- src/elements/osa coordinator/graphs/TopViolationNaturesByDepartment.jsx
+++ src/elements/osa coordinator/graphs/TopViolationNaturesByDepartment.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import ApexCharts from 'react-apexcharts';
 
-const TopViolationNaturesByDepartmentChart = () => {
+const TopViolationNaturesByDepartmentChart = ({ startDate, endDate }) => {
   const [state, setState] = useState({
     series: [
       {
-        data: [], // This will hold the violation nature data (e.g., x: nature_name, y: violation_count)
+        data: [], 
       },
     ],
     options: {
@@ -14,37 +14,42 @@ const TopViolationNaturesByDepartmentChart = () => {
       },
       chart: {
         height: 350,
-        type: 'treemap', // Change chart type to 'treemap'
+        type: 'treemap', 
       },
-      title: {
-        text: 'Top Violation Natures by Department', // Set title
-      },
+      colors: [
+        '#087830'
+      ], 
     },
   });
 
   useEffect(() => {
-    // Fetch top violation natures from the backend
-    fetch('http://localhost:9000/api/top-violationnatures')
-      .then((response) => response.json())
-      .then((data) => {
-        // Format the data for the treemap chart
-        const formattedData = data.map((record) => ({
-          x: record.nature_name, // Set 'x' as violation nature name
-          y: record.violation_count, // Set 'y' as the violation count
-        }));
+    const fetchData = () => {
+      const params = new URLSearchParams();
+      if (startDate) params.append('start_date', startDate);
+      if (endDate) params.append('end_date', endDate);
 
-        // Update the chart state with the formatted data
-        setState({
-          series: [{ data: formattedData }],
-          options: { ...state.options }, // Retain existing options
-        });
-      })
-      .catch((err) => console.error('Error fetching top violation natures:', err));
-  }, []);
+      // Fetch top violation natures from the backend
+      fetch(`http://localhost:9000/api/top-violationnatures?${params.toString()}`)
+        .then((response) => response.json())
+        .then((data) => {
+          const formattedData = data.map((record) => ({
+            x: record.nature_name, 
+            y: record.violation_count, 
+          }));
 
+          setState({
+            series: [{ data: formattedData }],
+            options: { ...state.options },
+          });
+        })
+        .catch((err) => console.error('Error fetching top violation natures:', err));
+    };
+    fetchData();
+  }, [startDate, endDate]);
+
+  
   return (
     <div>
-      <h2>Top Violation Natures</h2>
       {state.series[0].data.length > 0 ? (
         <ApexCharts
           options={state.options}
