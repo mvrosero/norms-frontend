fe46f94d14c70ce42c65b104f103b4564397dce6
Michaela Vera G. Rosero <mvrosero@gbox.ncf.edu.ph>
2025-01-26 21:52:53 +0800

    latetsttt

--- src/pages/osa coordinator/IndividualStudentRecord.jsx
+++ src/pages/osa coordinator/IndividualStudentRecord.jsx
@@ -154,14 +154,7 @@ return (
             </div>
 
             {/* Breadcrumbs and Tab Buttons */}
-            <div style={{     display: 'flex', 
-    justifyContent: 'space-between', 
-    marginBottom: '5px', 
-    width: '100%', // Take full width of the parent
-    maxWidth: '1200px', // Optionally, limit the max width
-    marginLeft: 'auto', // Align to the left or right
-    marginRight: 'auto',
-     marginBottom: '5px' }}>
+            <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '5px', width: '100%', maxWidth: '1000px', marginLeft: '120px', marginRight: 'auto', marginBottom: '5px' }}>
                 {/* Breadcrumbs */}
                 <nav style={{ flex: 1, display: 'flex', justifyContent: 'flex-start' }}>
                     <ol style={{ backgroundColor: 'transparent', padding: '0', margin: '0', listStyle: 'none', alignItems: 'center', display: 'flex', justifyContent: 'flex-start' }}>
