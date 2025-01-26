840b48749f48ffde8ff672a52ba396415f8421c9
Michaela Vera G. Rosero <mvrosero@gbox.ncf.edu.ph>
2025-01-26 15:42:58 +0800

    hi

--- src/elements/osa coordinator/tables/IndividualUniformDefianceTable.jsx
+++ src/elements/osa coordinator/tables/IndividualUniformDefianceTable.jsx
@@ -67,9 +67,6 @@ const IndividualUniformDefianceTable = ({ handleShowDetailsModal, filters, searc
     }, [headers]);
 
 
-
-
-
     // Set the styles for the status
     const renderStatus = (status) => {
         let backgroundColor, textColor;
@@ -157,6 +154,8 @@ const IndividualUniformDefianceTable = ({ handleShowDetailsModal, filters, searc
     };
 
     const renderPagination = () => {
+        if (loading) return null;
+        
         const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1);
     
         const buttonStyle = {
@@ -271,8 +270,19 @@ const renderTable = () => {
 
     const currentDefiances = filteredDefiances.slice(indexOfFirstDefiance, indexOfLastDefiance);
 
-    return (
-        <div style={{ paddingTop: '10px' }}>
+    // Show loading spinner when data is being fetched
+    if (loading) {
+        return (
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100%" }}>
+            <div style={{ width: "50px", height: "50px", border: "6px solid #f3f3f3", borderTop: "6px solid #a9a9a9", borderRadius: "50%", animation: "spin 1s linear infinite" }}></div>
+            <style> {`@keyframes spin { 0% { transform: rotate(0deg); } 100% { transform: rotate(360deg); } }`} </style>
+        </div>
+        );
+    }
+
+
+return (
+    <div style={{ paddingTop: '10px' }}>
             <Table bordered hover responsive style={{ borderRadius: '20px', marginBottom: '20px', marginLeft: '110px' }}>
                 <thead>
                     <tr>
@@ -375,7 +385,7 @@ const renderTable = () => {
 return (
     <div>
         {renderTable()}
-        {renderPagination()}
+        {!loading && renderPagination()}
     </div>
 );
 }
--- src/pages/osa coordinator/IndividualStudentRecord.jsx
+++ src/pages/osa coordinator/IndividualStudentRecord.jsx
@@ -147,7 +147,7 @@ return (
             </div>
 
             {/* Breadcrumbs */}
-            <nav style={{ width: '80%', marginBottom: '5px', marginLeft: '10px' }}>
+            <nav style={{ width: '80%', marginBottom: '5px', marginLeft: '30px' }}>
                 <ol style={{ backgroundColor: 'transparent', padding: '0', margin: '0', listStyle: 'none', alignItems: 'center', display: 'flex', justifyContent: 'flex-start' }}>
                     <li style={{ marginRight: '5px' }}>
                         <Link to="/coordinator-studentrecords" style={{ textDecoration: 'none', color: '#0D4809' }}>
