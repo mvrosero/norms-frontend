6e4e1a2f5d7d67f724d96cff21a3562cc776204e
Michaela Vera G. Rosero <mvrosero@gbox.ncf.edu.ph>
2025-01-30 20:32:05 +0800

    NORMS deployed final testing version 1.0

--- src/pages/administrator/AdminAccountHistory.jsx
+++ src/pages/administrator/AdminAccountHistory.jsx
@@ -81,9 +81,9 @@ export default function AdminAccountHistory() {
 
   if (loading) {
     return (
-      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100%" }}>
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
         <div style={{ width: "50px", height: "50px", border: "6px solid #f3f3f3", borderTop: "6px solid #a9a9a9", borderRadius: "50%", animation: "spin 1s linear infinite" }}></div>
-        <style> {`@keyframes spin { 0% { transform: rotate(0deg); } 100% { transform: rotate(360deg); } }`} </style>
+        <style>{`@keyframes spin { 0% { transform: rotate(0deg); } 100% { transform: rotate(360deg); } }`}</style>
       </div>
     );
   }
